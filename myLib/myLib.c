892ed3bb2e80ff48280982d8c1d99c053febcaec
Dae-Yong-Kim <youbinlove607@gmail.com>
2024-11-22 16:56:15 +0900

    Edit myLib And Create lcd1602 library file

--- myLib/myLib.c
+++ myLib/myLib.c
@@ -5,9 +5,8 @@
  *      Author: user
  */
 #include "main.h"
-#include "C:\Users\user\STM32Cube\Repository\STM32Cube_FW_F4_V1.28.1\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_i2c.h" //If you want to use it universally, You have to include "Absolute Path"
+//#include "C:\Users\user\STM32Cube\Repository\STM32Cube_FW_F4_V1.28.1\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_i2c.h" //If you want to use it universally, You have to include "Absolute Path"
 extern UART_HandleTypeDef huart2;
-I2C_HandleTypeDef *hi2c = NULL;
 
 // int __io_getchar(void) {}
 int __io_putchar(int ch) {
@@ -38,22 +37,4 @@ void Cursor(int x, int y) {
 	//sprintf(buf, "\033[%d;%dH", y, x);
 	//puts(buf);
 	//printf("%s", buf);
-}
-
-int i2c_init(I2C_HandleTypeDef *p) {
-	hi2c = p;
-}
-
-int i2c_scan() {
-	if(hi2c == NULL) return;
-	for(int addr = 0; addr < 128; addr++) {
-		if(HAL_I2C_IsDeviceReady(hi2c, addr, 1, 10) == HAL_OK) {
-			printf("  %02x", addr);
-		} else {
-			printf("  . ");
-		}
-		if((addr + 1) % 16 == 0) {
-			printf("\r\n");
-		}
-	}
 }
